ee7a15af027b8f9f92f9d1186727b20dba4daa9d
Tansu Dasli <tansudasli@gmail.com>
2019-06-16 18:21:26 +0300

    Initialize

--- install-kafka-as-standalone.sh
+++ install-kafka-as-standalone.sh
@@ -6,9 +6,10 @@ sudo apt install openjdk-8-jdk -y
 wget http://ftp.itu.edu.tr/Mirror/Apache/kafka/2.2.1/kafka_2.11-2.2.1.tgz
 
 tar -xvf kafka_2.11-2.2.1.tgz
-cd kafka_2.11-2.2.1
+cp -r kafka_2.11-2.2.1 ~/
+cd ~/kafka_2.11-2.2.1
 
-echo 'PATH="$PATH:/your/path/to/your/kafka/bin"' >> ~/.profile
+echo 'PATH="$PATH:~/kafka_2.11-2.2.1/bin"' >> ~/.profile
 source  ~/.profile
 
 echo "to test run, kafka_2.11-2.2.1/bin/kafka-topics.sh"
