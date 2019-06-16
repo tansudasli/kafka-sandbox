403ccd8b6bacb5e1e3647b25ed77e0b51c5cc6a1
Tansu Dasli <tansudasli@gmail.com>
2019-06-16 16:23:21 +0300

    Initialize

--- install-as-standalone-on-GCP.sh
+++ install-as-standalone-on-GCP.sh
@@ -1,3 +1,9 @@
+# create an instance on GCP
+# gcloud compute instances create kafka-instance-2 \
+#                                 --zone europe-west3-c \
+#                                 image-family ubuntu-1904
+
+
 wget https://www.apache.org/dyn/closer.cgi?path=/kafka/2.2.1/kafka_2.11-2.2.1.tgz
 
 tar -xvf kafka_2.11-2.2.1.tgz
@@ -9,4 +15,5 @@ echo 'PATH="$PATH:/your/path/to/your/kafka/bin"' >> ~/.profile
 source  ~/.profile
 
 echo "to test run, kafka_2.11-2.2.1/bin/kafka-topics.sh"
+
 
