0dc8e7c4f0dc32c26260dadfd9d68eb63ff635e7
Tansu Dasli <tansudasli@gmail.com>
2019-06-19 12:48:14 +0300

    Update - kafka Java APIs, config params changed

--- kafka-basics/src/main/java/com/kafka/Producer.java
+++ kafka-basics/src/main/java/com/kafka/Producer.java
@@ -1,7 +1,9 @@
 package com.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,16 +20,16 @@ public class Producer {
     private static Logger logger = LoggerFactory.getLogger(Producer.class);
 
     private static Map<String, String> products = Stream.of(new String[][] {
-            {"clothes", "id:100, category:clothes, price:1000"},
-            {"watches", "id:100, category:watches, price:500"},
-            {"clothes", "id:100, category:clothes, price:100"},
-            {"shoes", "id:100, category:shoes, price:250, brand:nike"},
-            {"shoes", "id:100, category:shoes, price:1000, brand: adidas"},
-            {"shoes", "id:100, category:shoes, price:1000, brand: adidas"},
-            {"clothes", "id:100, category:clothes, price:400, brand:gucci"}
+            {"clothes:100", "id:100, category:clothes, price:1000"},
+            {"watches:200", "id:200, category:watches, price:500"},
+            {"clothes:300", "id:300, category:clothes, price:100"},
+            {"shoes:400", "id:400, category:shoes, price:250, brand:nike"},
+            {"shoes:500", "id:500, category:shoes, price:1000, brand: adidas"},
+            {"shoes:600", "id:600, category:shoes, price:1000, brand: adidas"},
+            {"clothes:700", "id:700, category:clothes, price:400, brand:gucci"}
     }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Properties properties = new Properties();
 
@@ -40,21 +42,28 @@ public class Producer {
             logger.error("kafka-server.properties file not loaded", e);
         }
 
+        properties.putAll(Stream.of(new String[][] {
+                {ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getProperty("bootstrap.servers")},
+                {ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()},
+                {ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()}
+        }).collect(Collectors.toMap(data -> data[0], data -> data[1])));
+
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         products.forEach((k, v) -> {
 
-            ProducerRecord<String, String> record = new ProducerRecord<>(properties.getProperty("topic"), k, v);
+            ProducerRecord<String, String> record = new ProducerRecord<>(properties.getProperty("topic"), k.split(":")[0], v);
 
             producer.send(record, (metadata, exception) -> {
 
                 if (exception == null)
-                    logger.info(String.format("Received new message: on topic %s\ntimestamp: %d\npartition: %d\noffset: %d", metadata.topic(), metadata.timestamp(), metadata.partition(), metadata.offset()));
+                    logger.info(String.format("Received new message: on topic %s\ntimestamp: %d\npartition: %d\noffset: %d",
+                                                metadata.topic(), metadata.timestamp(), metadata.partition(), metadata.offset()));
                 else
                     logger.error("Received new message:", exception);
             });
-
-            producer.close();
         });
+
+        producer.close();
     }
 }
